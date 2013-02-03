f82090456ad997b1062be9ff9e3beebbf3340bd8
Josh <josh@allensw.com>
2013-02-03 16:18:56 -0500

    strobe at 60hz

---
+++ Strobe/test.c
@@ -0,0 +1,61 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
+#include <string.h>
+#include <avr/sfr_defs.h>
+
+static void setup_clock()
+{
+	CLKPR = 0x80;	/*Setup CLKPCE to be receptive*/
+	CLKPR = 0x00; //no divisor
+}
+
+static void setup_timers()
+{
+
+	TCCR1A = _BV(COM1A0);//Toggle OC1A
+	TCCR1B = _BV(WGM12) | _BV(CS12); //CTC, 256 divisor
+	OCR1A = 1041; //60hz
+	TIMSK1 = 0x00; //no interrupts
+}
+
+int main( void )
+{
+	cli();
+
+	DDRC = _BV(PC6); //output pin
+
+	setup_clock();
+	setup_timers();
+
+	sei();
+
+	while(1);
+
+	return 0;
+}
+
+/*
+
+Copyright (c) 2013 Joshua Allen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
