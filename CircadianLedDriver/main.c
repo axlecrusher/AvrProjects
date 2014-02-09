3003ebd72430417c248cc74a251bf9c7b572e9dd
Josh <josh@allensw.com>
2014-02-09 17:53:18 -0500

    add code to tween color over time

--- CircadianLedDriver/main.c
+++ CircadianLedDriver/main.c
@@ -27,17 +27,18 @@ volatile char updateAnimate = 0; //time of day in seconds
 ISR(TIM0_OVF_vect)
 {
 	//triggers every 256 clock ticks
-	//clock runs at 5Mhz
+	//PWM clock runs at 2.5Mhz
 	++cx;
-	if (cx >= 19531) //overflows per second 19531.25
+	if (cx >= 9765) //overflows per second 9765.625
 	{
 		cx = 0;
 		++time;
 		++drift;
-		if (drift>=4)
+		if (drift>=8)
 		{
 			//account for drift from uneven division of HZ
-			++time;
+			cx+=5;
+//			++time;
 			drift = 0;
 		}
 		if (time >= 86400) time = 0; //reset time ever 24 hours
@@ -47,17 +48,43 @@ ISR(TIM0_OVF_vect)
 	}
 }
 
-void Animate()
-{
-
-
+inline uint8_t interpolate(uint8_t start, uint8_t end, uint8_t p) {
+	uint16_t x= (end*p)>>8;
+	uint16_t y= (start * (256-p))>>8;
+	return x+y;
 }
 
+uint8_t ComputePercent(uint32_t x, uint32_t y, uint32_t z)
+{
+	y -= x;
+	z -= x; //geater than 0
+	uint32_t p = (z<<8)/y;//conver to a range between 0 and 255
+	return (uint8_t)p;	
+}
+
+uint8_t ComputeTween(uint8_t begin, uint8_t end, uint32_t startTime, uint32_t endTime)
+{
+	if (time<=startTime) return begin;
+	if (time>=endTime) return end;
+
+	uint8_t p = ComputePercent(startTime,endTime,time);
+
+	return interpolate(begin,end,p);
+}
+
+
+void Animate()
+{
+	SetR(ComputeTween(0,255,0,600));
+	SetG(ComputeTween(0,255,600,601));
+	updateAnimate = 0;
+}
 
 static void setup_clock()
 {
 	CLKPR = 0x80;	/*Setup CLKPCE to be receptive*/
-	CLKPR = _BV(CLKPS1); //4
+//	CLKPR = _BV(CLKPS1); //4
+	CLKPR = 0x00; //fullspeed
 }
 
 void DisablePWM()
@@ -113,7 +140,8 @@ static void setup_pwm()
 {
 	//FAST PWM running at cpu speed
 	TCCR0A = _BV(WGM01) | _BV(WGM00);
-	TCCR0B = _BV(CS00);//  | _BV(CS01);
+//	TCCR0B = _BV(CS00);//  | _BV(CS01);
+	TCCR0B = _BV(CS01);
 
 	//use overflow interrupt as timer to count time that has passed
 	TIMSK0 = _BV(TOIE0);
@@ -142,7 +170,8 @@ OCR0A = 0;
 */
 	//FAST PWM at cpu speed, 8 bit
 	TCCR1A = _BV(WGM10);
-	TCCR1B = _BV(WGM12) | _BV(CS10);
+//	TCCR1B = _BV(WGM12) | _BV(CS10);
+	TCCR1B = _BV(WGM12) | _BV(CS11);
 //	OCR1B = 0;
 /*
 	TCCR1A = 0;
