d4eecb7f01c23af76a8c97d09ec7d980a4578120
Josh Allen <josh@allensw.com>
2015-03-04 22:18:35 -0500

    add file for usb protocl defines

---
+++ TelescopeFirmware/usb_protocol.h
@@ -0,0 +1,12 @@
+#ifndef USB_PROTOCOL_H
+#define USB_PROTOCOL_H
+
+#define PING 0xA1
+#define PONG 0xA2
+#define MOTOR_INFO 0xA3
+#define MOTORS_ON 0xA4
+#define MOTORS_OFF 0xA5
+#define GOTO_Y 0xA6
+#define GOTO_X 0xA7
+
+#endif
